af7e88e31d18ddb594bccc6c3a4cbea72b0cd0a5
Hryhoriev Vladislav <wladislav101@gmail.com>
2024-10-25 13:14:44 +0300

    fix_token

--- src/app/menu/layout.tsx
+++ src/app/menu/layout.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [token, setToken] = useState<string | null>(null);
 
-  useEffect(() => setToken(localStorage.getItem("token")), []);
+  useEffect(() => setToken(localStorage.getItem("token_in")), []);
 
   return <>{token && children}</>;
 };
