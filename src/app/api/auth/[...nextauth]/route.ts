84fd05d1db0fd14cdf4894f1cf2aca75521f4fa2
Hryhoriev Vladislav <wladislav101@gmail.com>
2025-02-06 14:58:04 +0200

    Рефакторинг аутентификации: удаление закомментированного кода, изменение обработки ошибок и экспорт обработчика для GET и POST запросов.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,8 @@
-// import { cookies } from "next/headers";
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function POST(req: NextRequest) {
-//   try {
-//     const { authToken } = await req.json();
-//     cookies().set("auth-token", authToken, {
-//       httpOnly: true,
-//       maxAge: 31 * 24 * 60 * 60 * 1000,
-//     });
-
-//     console.log(cookies().get("auth-token"));
-
-//     return NextResponse.json({ ok: true }, { status: 200 });
-//   } catch (error) {
-//     console.log(error);
-//     return NextResponse.json({ ok: false, message: error }, { status: 500 });
-//   }
-// }
 import { users } from "@/users";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "credentials",
@@ -32,7 +13,7 @@ export const authOptions: NextAuthOptions = {
         if (!credentials?.authToken) throw new Error("Token is required");
 
         const token = users.find((u) => u.authToken === credentials.authToken);
-        if (!token) return null;
+        if (!token) throw new Error("Token not found");
 
         return { id: token.id, name: token.name };
       },
@@ -43,4 +24,6 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
