f34f15df2f74021119434fea01e0e927129e8877
Hryhoriev Vladislav <wladislav101@gmail.com>
2025-02-10 17:41:41 +0200

    Упрощение доступа к данным в API Nova Poshta: удалено лишнее обращение к json.data, улучшена читаемость кода.

--- src/app/api/nova-poshta/route.ts
+++ src/app/api/nova-poshta/route.ts
@@ -1,4 +1,4 @@
-import { API_URLS, LINKS } from "@/constants";
+import { LINKS } from "@/constants";
 import { DeliveryInfoResponse, DeliveryResponse } from "@/lib/types";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -23,19 +23,17 @@ export async function POST(req: NextRequest) {
     });
 
     const json: DeliveryInfoResponse = await response.json();
+    const data = json?.data[0];
 
-    const deliveryTime = json.data[0].ActualDeliveryDate.split(" ")[1]?.slice(
-      0,
-      5,
-    );
+    const deliveryTime = data.ActualDeliveryDate.split(" ")[1]?.slice(0, 5);
 
     const [deliveryYear, deliveryMonth, deliveryDay] =
-      json.data[0].ActualDeliveryDate.split(" ")[0].split("-");
+      data.ActualDeliveryDate.split(" ")[0].split("-");
 
     const isValidDelivery = deliveryDay && deliveryMonth && deliveryYear;
 
     const [returnYear, returnMonth, returnDay] =
-      json.data[0]?.DateReturnCargo?.split("-") || [];
+      data?.DateReturnCargo?.split("-") || [];
 
     const isValid = returnDay && returnMonth && returnYear;
 
