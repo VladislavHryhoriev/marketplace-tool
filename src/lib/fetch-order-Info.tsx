6feaaa4060bd129d082db7704e18a5620b9b7ec7
Hryhoriev Vladislav <wladislav101@gmail.com>
2025-02-22 17:58:20 +0200

    hotfix

--- src/lib/fetch-order-Info.tsx
+++ src/lib/fetch-order-Info.tsx
@@ -1,10 +1,10 @@
 import { TemplateNames } from "@/consts/TEMPLATES";
-import { getOrderInfoEpicentr } from "@/lib/epicentr/get-order-info";
-import { getTrackingInfo } from "@/lib/get-tracking-info";
 import { getOrderInfoRozetka } from "@/lib/rozetka/get-order-info";
 import { getTemplateEpicentr } from "@/lib/templates/get-template-epicentr";
 import { getTemplateRozetka } from "@/lib/templates/get-template-rozetka";
 import { toast } from "react-toastify";
+import { getOrderInfoEpicentr } from "./epicentr/get-order-info";
+import { getTrackingInfo } from "./get-tracking-info";
 
 export const fetchOrderInfo = async (
   type: TemplateNames,
